e9096fd77bffdac31b6919158921f0df4bcbe298
NamanJain1902 <1902ben10@gmail.com>
2021-12-15 23:07:32 +0530

    fixed faling test

--- average.js
+++ average.js
@@ -1,23 +1,13 @@
 function average(numbers) {
   const numbersWithoutNaN = numbers.filter(val => !Number.isNaN(val))
   const numbersWithoutNaNCount = numbersWithoutNaN.length
-  const nanCountPercentage = (numbersWithoutNaNCount / numbers.length);
+  const numbersCountPercentage = (numbersWithoutNaNCount / numbers.length);
 
-  if(nanCountPercentage > 0.6){
+  if (numbersCountPercentage > 0.4){
+    return numbersWithoutNaN.reduce((p, c)=> p + c, 0) /numbersWithoutNaNCount;
+  } else{
     return NaN;
-  } 
- //  else if(nanCountPercentage >= 0.3 && nanCountPercentage <= 0.6){
-   
- //   let half = Math.floor( numbersWithoutNaNCount/ 2);
- //   const numbersWithoutNaNMedian = (numbersWithoutNaNCount % 2) 
- //           ? numbersWithoutNaN[half] 
- //           : (numbersWithoutNaN[half - 1] + numbersWithoutNaN[half]) / 2.0;
-
- //   return (numbersWithoutNaN.reduce((p, c)=> p + c, 0) + 
- //                    numbersWithoutNaNMedian) / numbers.length;
- //  }
-  
-  return numbersWithoutNaN.reduce((p, c)=> p + c, 0) / numbersWithoutNaNCount;
+  }
 }
 
 module.exports = {average};
