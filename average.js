f04ef3896311ad0f96f5f591d6c7880982fcde1d
NamanJain1902 <1902ben10@gmail.com>
2021-12-15 23:00:05 +0530

    fixed commented code allowed limit test fail

--- average.js
+++ average.js
@@ -5,16 +5,17 @@ function average(numbers) {
 
   if(nanCountPercentage > 0.6){
     return NaN;
-  }
-  // else if(nanCountPercentage >= 0.3 && nanCountPercentage <= 0.6){
+  } 
+ //  else if(nanCountPercentage >= 0.3 && nanCountPercentage <= 0.6){
    
-  //   let half = Math.floor( numbersWithoutNaNCount/ 2);
-  //   const numbersWithoutNaNMedian = (numbersWithoutNaNCount % 2) 
-  //                                   ? numbersWithoutNaN[half] 
-  //                                   : (numbersWithoutNaN[half - 1] + numbersWithoutNaN[half]) / 2.0;
+ //   let half = Math.floor( numbersWithoutNaNCount/ 2);
+ //   const numbersWithoutNaNMedian = (numbersWithoutNaNCount % 2) 
+ //           ? numbersWithoutNaN[half] 
+ //           : (numbersWithoutNaN[half - 1] + numbersWithoutNaN[half]) / 2.0;
 
-  //   return (numbersWithoutNaN.reduce((p, c)=> p + c, 0) + numbersWithoutNaNMedian) / numbers.length;
-  // }
+ //   return (numbersWithoutNaN.reduce((p, c)=> p + c, 0) + 
+ //                    numbersWithoutNaNMedian) / numbers.length;
+ //  }
   
   return numbersWithoutNaN.reduce((p, c)=> p + c, 0) / numbersWithoutNaNCount;
 }
